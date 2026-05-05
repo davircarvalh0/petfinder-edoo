305b5729e8908c462efeebb6d699dc1b4802f708
GabrielGodoy-ggcm <ggcm@cin.ufpe.br>
2026-05-04 23:47:14 -0300

    feat/add fotos no backend

--- src/models/Animal.h
+++ src/models/Animal.h
@@ -9,23 +9,23 @@ private:
     string cor;
     float peso;
     int idade;
+    string foto;
 public:
 //inicializa todos os atributos
 //o "const string&" recebe a string sem copiar, mais eficiente
-    Animal(const string& nome,
-           const string& cor,
-           float peso,
-           int idade);
+    Animal(const string& nome, const string& cor,float peso, int idade, const string& foto);
     //getters
     string getNome() const;
     string getCor() const;
     float getPeso() const;
     int getIdade() const;
+    string getFoto();
     //setters
     void setNome(const string& nome);
     void setCor(const string& cor);
     void setPeso(float peso);
     void setIdade(int idade);
+    void setFoto(string foto);
     //metodo abstrato, cada animal implementa o seu
     virtual void exibir() const = 0;
     //destrutor virtual 
