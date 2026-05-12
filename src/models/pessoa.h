c37e1cd15b52cad75c5cb011d711e6ee1e8ae30c
João Felipe <joaofelipe10009@gmail.com>
2026-05-12 02:12:06 -0300

    Comentarios

--- src/models/pessoa.h
+++ src/models/pessoa.h
@@ -5,23 +5,22 @@ using namespace std;
 class Pessoa {
 protected:     //protected faz com q outras subclasses acessem tambem
                 // private (oq estava antes) apenas csse pessoa acessava
-    string nome;
-    string cpf;
-    string telefone;
-    string email;
+    string nome;        //nome completo da pessoa
+    string cpf;         //cpf usado como identificador unico
+    string telefone;    //telefone para contato
+    string email;       //email para contato
 
 public:
-    Pessoa(string n, string c, string t, string e) 
-        : nome(n), cpf(c), telefone(t), email(e) {}
+    Pessoa(string n, string c, string t, string e);   //construtor
 
-    string getNome() const;
-    string getCpf() const;
-    string getTelefone() const;
-    string getEmail() const;
+    string getNome() const;      //retorna o nome
+    string getCpf() const;       //retorna o cpf
+    string getTelefone() const;  //retorna o telefone
+    string getEmail() const;     //retorna o email
     
-    virtual ~Pessoa() {}
+    virtual ~Pessoa() {}    //destrutor virtual faz que o destrutor correto seja chamado nas subclasses
 
     
     
-    virtual void exibirInfo() = 0; 
+    virtual void exibirInfo() = 0;      //metodo virtual puro obriga Dono e Usuario a terem sua propria versao
 };
